ccd12c92c07ed4d0f0e94a89dea36348230cde4b
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2026-02-07 07:52:15 -0500

    feat: setup i18n with EN, FR, DE, PT support

---
+++ src/i18n.ts
@@ -0,0 +1,26 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import en from './locales/en.json';
+import fr from './locales/fr.json';
+import de from './locales/de.json';
+import pt from './locales/pt.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      fr: { translation: fr },
+      de: { translation: de },
+      pt: { translation: pt },
+    },
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
